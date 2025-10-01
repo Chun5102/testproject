ac31cc5baa10acc53ed9d2b67010be76ccb1c111
DESKTOP-P4UUKAO\student <kim1475369h@gmail.com>
2025-10-01 21:42:34 +0800

    ChangColor.vue

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,7 @@ import HelloWorld from './components/HelloWorld.vue'
 import TheWelcome from './components/TheWelcome.vue'
 import RefCount from './components/RefCount.vue'
 import ReactiveComp from './components/ReactiveComp.vue'
+import ChangColor from './components/ChangColor.vue'
 </script>
 
 <template>
@@ -13,6 +14,7 @@ import ReactiveComp from './components/ReactiveComp.vue'
       <!-- <HelloWorld msg="did it!" /> -->
       <!-- <RefCount /> -->
       <ReactiveComp />
+      <ChangColor />
     </div>
   </header>
 
